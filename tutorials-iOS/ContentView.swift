b59d6c359f439a70605584d8f1907875d016bc07
AFURO-1130 <abcshotaro616@gmail.com>
2023-04-16 10:03:37 +0900

    Initial Commit

---
+++ tutorials-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutorials-iOS
+//
+//  Created by 金城翔太郎 on 2023/04/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tutorials-iOS/tutorials_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorials_iOSApp.swift
+//  tutorials-iOS
+//
+//  Created by 金城翔太郎 on 2023/04/16.
+//
+
+import SwiftUI
+
+@main
+struct tutorials_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
